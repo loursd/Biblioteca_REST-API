3a6b28adb94182cd1e87e706781b66a14813d6c0
Lourdes Duarte <117114460+loursd@users.noreply.github.com>
2023-10-26 00:12:26 -0300

    Corrección rutas delete por id y por isbn

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ import { libro } from './controller.js';
 export const router = Router()
 
 router.get('/libros', libro.getAll);
+router.get('/libro', libro.getOne); //llamar solo un libro por id
 router.post('/libro', libro.add);
-router.delete('/libro', libro.delete);
-router.put('/libro', libro.update);
-router.get('/libro', libro.getOne) //llamar solo un libro por id
+router.delete('/libro', libro.deleteID); //elimina libro por ID
+router.delete('/libro', libro.deleteISBN); //elimina libro por ISBN
+router.put('/libro', libro.update); //actualiza libro por ISBN
