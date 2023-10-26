a6a8d0bb1cdda3a3285b8b726ad22ac47c762517
Lourdes Duarte <117114460+loursd@users.noreply.github.com>
2023-10-26 01:00:27 -0300

    Actualización base de datos

--- script/biblioteca.sql
+++ script/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-10-2023 a las 03:25:19
+-- Tiempo de generación: 26-10-2023 a las 05:59:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -45,7 +45,8 @@ INSERT INTO `libros` (`id`, `nombre`, `autor`, `categoria`, `aniopublicacion`, `
 (2, 'Probabilidad y Aplicaciones', 'Paul L. Meyer', 'Técnico', '1973-01-03', '0201518775'),
 (3, 'La isla de Eudamon', 'Leandro Calderone', 'Novela', '2010-01-11', '9789504924623'),
 (4, 'El curioso incidente del perro', 'Mark Haddon', 'Novela', '2004-01-09', '9788478889105'),
-(5, 'Cuentos de los Hnos Grimm', 'Guillermo Grimm', 'Cuentos Infantiles', '2007-01-02', '9789871368259');
+(5, 'Los Miserables', 'Victor Hugo', 'Ficción', '1862-06-01', '9788491051121'),
+(6, 'Un ataque de lucidez', 'Jill Bolte Taylor', 'Biografía', '2009-01-23', '9788499927275');
 
 --
 -- Índices para tablas volcadas
@@ -65,7 +66,7 @@ ALTER TABLE `libros`
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
