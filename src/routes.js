f50d12eb51f30162565780fbf1a93fc8fabacec7
Lourdes Duarte <117114460+loursd@users.noreply.github.com>
2023-11-06 18:18:31 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,9 @@ import { libro } from './controller.js';
 
 export const router = Router()
 
-router.get('/libros', libro.getAll);
+router.get('/libros', libro.getAll); //traer todos los libros
 router.get('/libro/:id', libro.getOne); //llamar solo un libro por id
-router.post('/libro', libro.add);
+router.post('/libro', libro.add); //añadir nuevo libro
 router.delete('/libro/id', libro.deleteID); //elimina libro por ID
 router.delete('/libro/ISBN', libro.deleteISBN); //elimina libro por ISBN
 router.put('/libro', libro.update); //actualiza libro por id
